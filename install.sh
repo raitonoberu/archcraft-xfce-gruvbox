6b8f451f92e2183b1dabdcfdf9356885f366ef5e
Denis <raitonoberu@mail.ru>
2021-09-22 23:32:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo git clone https://github.com/jmattheis/gruvbox-dark-gtk /usr/share/themes/g
 
 echo "Cloning Gruvbox icons..."
 sudo git clone https://github.com/jmattheis/gruvbox-dark-icons-gtk /usr/share/icons/gruvbox
+sudo gtk-update-icon-cache /usr/share/icons/gruvbox
 
 echo "Clonning setting script..."
 sudo curl https://raw.githubusercontent.com/raitonoberu/archcraft-xfce-gruvbox/master/Gruvbox.sh --output /usr/share/archcraft/styles/Gruvbox.sh
