f3817308338f80abd7a853fd32680a7bc39faccc
Denis <raitonoberu@mail.ru>
2021-09-22 23:21:19 +0700

    Use sudo instead of requesting su

--- install.sh
+++ install.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-if [[ $EUID > 0 ]]
-  then echo "Please run as root."
-  exit
-fi
-
 echo "Cloning Gruvbox theme..."
-git clone https://github.com/jmattheis/gruvbox-dark-gtk /usr/share/themes/gruvbox
+sudo git clone https://github.com/jmattheis/gruvbox-dark-gtk /usr/share/themes/gruvbox
 
 echo "Cloning Gruvbox icons..."
-git clone https://github.com/jmattheis/gruvbox-dark-icons-gtk /usr/share/icons/gruvbox
+sudo git clone https://github.com/jmattheis/gruvbox-dark-icons-gtk /usr/share/icons/gruvbox
 
-echo "Cloning a configuration script..."
-curl https://raw.githubusercontent.com/raitonoberu/archcraft-xfce-gruvbox/master/Gruvbox.sh --output /usr/share/archcraft/styles/Gruvbox.sh
+echo "Clonning setting script..."
+sudo curl https://raw.githubusercontent.com/raitonoberu/archcraft-xfce-gruvbox/master/Gruvbox.sh --output /usr/share/archcraft/styles/Gruvbox.sh
 
 echo "Setting up the theme..."
 /usr/share/archcraft/styles/Gruvbox.sh
